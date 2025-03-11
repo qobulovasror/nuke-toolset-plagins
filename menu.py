8c40ad6acdb5f0e851059972f628034b171ceb46
qobulovasror <qobulovasror0@gmail.com>
2025-03-11 17:38:17 +0500

    added keentools

--- menu.py
+++ menu.py
@@ -5,8 +5,7 @@ import os
 toolbar = nuke.menu("Nodes")
 toolbar.addCommand("Merge/Premult", "nuke.createNode('Premult')", "[", icon="Premult.png")
 toolbar.addCommand("Time/FrameHold", "nuke.createNode('FrameHold')", "h", icon="FrameHold.png")
-#toolbar.addCommand("Transform/Tracker", "nuke.createNode('Tracker4')", "SHIFT+T", icon="Tracker.png")
-toolbar.addCommand("3D/3D Classic/CameraTracker", "nuke.createNode('CameraTracker')", "C", icon="CameraTracker.png")
+toolbar.addCommand("3D/3D Classic/CameraTracker", "nuke.createNode('CameraTracker')", "SHIFT+C", icon="CameraTracker.png")
 
 
 # Get the existing Nuke "Transform" menu
